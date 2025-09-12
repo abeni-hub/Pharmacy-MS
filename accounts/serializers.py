2289330c0c7ea6cc7ae66319b140a5bab49b5449
abeni <abeni@email.com>
2025-09-12 00:28:24 -0700

    add registeration function on the accounts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,25 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import get_user_model
 
+
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'role']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # hash password
+        user.save()
+        return user
+    
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,33 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 from .models import User
 from .serializers import UserSerializer
+from .serializers import RegisterSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken    
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # Generate JWT token after registration
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "user": serializer.data,
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+    
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
