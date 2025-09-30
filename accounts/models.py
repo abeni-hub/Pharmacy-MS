f413a43004d88c9da3ef2b629a65cf3ce791b405
abeni <abeni@email.com>
2025-09-29 23:52:54 -0700

    add uuid

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
 class User(AbstractUser):
-    ROLE_CHOICES = (
-        ('admin', 'Admin'),
-        ('pharmacist', 'Pharmacist'),
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
     )
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='pharmacist')
-
+    role = models.CharField(max_length=50, choices=[("admin", "Admin"), ("pharmacist", "Pharmacist")])
     def __str__(self):
         return f"{self.username} ({self.role})"
